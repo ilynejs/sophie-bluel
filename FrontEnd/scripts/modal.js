f5daade925a97f88afa40e337bb3adb489ded330
Ilyne <ilynejs@proton.me>
2024-10-03 14:43:49 +0200

    Création de la modale d'ajout de photo
    - Validation du formulaire.
    - Vérification du type de fichier et vérification de la taille du fichier.
    - Prévisualisation de l'image ajoutée.
    
    Mise à jour du DOM.

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -1,3 +1,6 @@
+import { fetchWorks } from './dataFetching.js';
+import { displayWorks } from './display.js';
+
 const hideModal = () => {
 	const modalDiv = document.querySelector('#modals');
 	modalDiv.style.display = 'none';
@@ -14,8 +17,11 @@ const showModal = () => {
 	modalOverlay.style.display = 'block';
 };
 
-const buildWorkModal = (works) => {
+/* Modale de gestion des travaux */
+const buildWorkModal = (works, categories) => {
 	const modalOverlay = document.querySelector('.modal-overlay');
+	modalOverlay.innerHTML = '';
+
 	showModal();
 
 	modalOverlay.addEventListener('click', (event) => {
@@ -42,7 +48,7 @@ const buildWorkModal = (works) => {
 
                 <hr class="modal-divider" />
 
-                <button class="add-btn">Ajouter une photo</button>
+                <button class="switch-add-btn">Ajouter une photo</button>
             </div>
         </section>
     `;
@@ -58,10 +64,9 @@ const buildWorkModal = (works) => {
 		hideModal();
 	});
 
-	const addPhotoBtn = modalOverlay.querySelector('.add-btn');
+	const addPhotoBtn = modalOverlay.querySelector('.switch-add-btn');
 	addPhotoBtn.addEventListener('click', () => {
-		// À faire : ajouter la modale de l'ajout de photo
-		console.log('Ajouter une photo');
+		buildAddPhotoModal(categories);
 	});
 };
 
@@ -109,7 +114,6 @@ const deleteWork = async (workId, item) => {
 		});
 		if (res.ok) {
 			item.remove();
-
 			removeWorkFromGallery(workId);
 		} else {
 			console.error('Erreur lors de la suppression du travail');
@@ -131,4 +135,166 @@ const removeWorkFromGallery = (workId) => {
 	}
 };
 
-export { buildWorkModal };
+/* Modale d'ajout de photo */
+const buildAddPhotoModal = (categories) => {
+	const modalOverlay = document.querySelector('.modal-overlay');
+	modalOverlay.innerHTML = '';
+
+	const modalContent = document.createElement('section');
+	modalContent.classList.add('modal', 'add-photo-modal');
+
+	modalContent.innerHTML = `
+		<header class="modal-header">
+			<div class="actions">
+				<button class="back-btn">
+					<img src="./assets/icons/arrow-left.png" alt="icone de retour" />
+				</button>
+				<button class="close-btn">
+					<img src="./assets/icons/close.png" alt="icone de fermeture" />
+				</button>
+			</div>
+			<h3>Ajouter une photo</h3>
+		</header>
+		<div class="modal-content">
+			<form id="add-photo-form" method="post">
+				<label class="image-label">
+					<img src="./assets/icons/picture_placeholder.png" alt="placeholder de l'image" />
+					<input type="file" name="image" id="image-input" required />
+					<div class="fake-button">+ Ajouter photo</div>
+					<span class="details">jpg, png : 4mo max</span>
+                    <img class="image-preview" src="" alt="preview de l'image" />
+				</label>
+                <p class="error"></p>
+				<label>
+					Titre
+					<input type="text" name="title" id="title" required />
+				</label>
+				<label>
+					Catégorie
+					<select name="category" id="category" required>
+						<option value="" selected disabled hidden></option>
+					</select>
+				</label>
+				<hr class="modal-divider" />
+				<input type="submit" value="Valider" class="disabled" />
+			</form>
+		</div>
+	`;
+
+	modalOverlay.appendChild(modalContent);
+
+	const categorySelect = modalContent.querySelector('#category');
+	for (const category of categories) {
+		const option = document.createElement('option');
+		option.value = category.id;
+		option.textContent = category.name;
+		categorySelect.appendChild(option);
+	}
+
+	const closeModalBtn = modalContent.querySelector('.close-btn');
+	const backBtn = modalContent.querySelector('.back-btn');
+
+	closeModalBtn.addEventListener('click', () => {
+		hideModal();
+	});
+	backBtn.addEventListener('click', async () => {
+		const works = await fetchWorks();
+		buildWorkModal(works, categories);
+	});
+
+	setupImageUpload(modalContent);
+
+	const submitBtn = modalContent.querySelector('input[type="submit"]');
+	const form = document.querySelector('#add-photo-form');
+
+	form.addEventListener('change', () => {
+		if (isFormValid(form)) {
+			submitBtn.classList.remove('disabled');
+		} else {
+			submitBtn.classList.add('disabled');
+		}
+	});
+
+	form.addEventListener('submit', async (event) => {
+		event.preventDefault();
+		const formData = new FormData(form);
+		const user = JSON.parse(localStorage.getItem('user'));
+
+		try {
+			const res = await fetch('http://localhost:5678/api/works', {
+				method: 'POST',
+				body: formData,
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			});
+			if (res.ok) {
+				const works = await fetchWorks();
+				displayWorks(works);
+
+				form.reset();
+				hideModal();
+			} else {
+				console.error("Erreur lors de l'ajout de la photo");
+			}
+		} catch (error) {
+			console.error('Erreur:', error);
+		}
+	});
+};
+
+const setupImageUpload = (modalContent) => {
+	const imagePreview = document.querySelector('.image-preview');
+	const imageInput = modalContent.querySelector('#image-input');
+	const placeholderImg = modalContent.querySelector(
+		`.image-label img[alt="placeholder de l'image"]`
+	);
+	const fakeButton = modalContent.querySelector('.fake-button');
+	const detailsText = modalContent.querySelector('.details');
+
+	imageInput.addEventListener('change', (event) => {
+		const file = event.target.files[0];
+		const errorElement = document.querySelector('.error');
+		errorElement.textContent = '';
+
+		// Vérification du type de fichier
+		const validTypes = ['image/jpeg', 'image/png'];
+		if (file && !validTypes.includes(file.type)) {
+			errorElement.textContent =
+				'Le fichier doit être au format jpg ou png.';
+			imageInput.value = '';
+			return;
+		}
+
+		// Vérfication de la taille du fichier
+		const maxSize = 4 * 1024 * 1024;
+		if (file && file.size > maxSize) {
+			errorElement.textContent = 'Le fichier doit être inférieur à 4 Mo.';
+			imageInput.value = '';
+			return;
+		}
+
+		if (file) {
+			const imageUrl = URL.createObjectURL(file);
+
+			imagePreview.src = imageUrl;
+			imagePreview.style.display = 'block';
+
+			placeholderImg.style.display = 'none';
+			fakeButton.style.display = 'none';
+			detailsText.style.display = 'none';
+
+			errorElement.textContent = '';
+		}
+	});
+};
+
+const isFormValid = (form) => {
+	return (
+		form.querySelector('#image-input').files.length > 0 &&
+		form.querySelector('#title').value !== '' &&
+		form.querySelector('#category').value !== ''
+	);
+};
+
+export { buildWorkModal, buildAddPhotoModal };
